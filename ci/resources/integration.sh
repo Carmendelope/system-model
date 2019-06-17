3ea0ff1f347d787701015a9788dd3e0909d19bb2
Rodrigo Núñez <rnunez@daisho.group>
2019-06-17 11:53:39 +0200

    Moved dep ensure out of Dockerfile

--- ci/resources/integration.sh
+++ ci/resources/integration.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd /go/src/github.com/nalej/system-model
-dep ensure > /dev/null 2>&1
 go test -v -coverprofile=coverage.out -covermode=atomic ./... 2>&1
 echo "---####--- COVERAGE FILE CONTENTS ---####---"
 cat coverage.out
