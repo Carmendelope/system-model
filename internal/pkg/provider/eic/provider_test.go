2fa3bb204cc10dd795d7e7e6b5b2774db033ed0b
Rodrigo Núñez <rnunez@daisho.group>
2019-06-28 13:34:15 +0200

    Edge controller provider tests clean up

--- internal/pkg/provider/eic/provider_test.go
+++ internal/pkg/provider/eic/provider_test.go
@@ -5,6 +5,8 @@
 package eic
 
 import (
+	"os"
+
 	"github.com/nalej/system-model/internal/pkg/entities"
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
@@ -13,70 +15,101 @@ import (
 func RunTest(provider Provider) {
 
 	ginkgo.BeforeEach(func() {
-		provider.Clear()
+		var clearProvider = os.Getenv("IT_CLEAR_PROVIDER")
+		if clearProvider == "true" {
+			provider.Clear()
+		}
 	})
 
-	ginkgo.It("should be able to add a controller", func(){
+	ginkgo.It("should be able to add a controller", func() {
 		toAdd := CreateTestEdgeController()
+
 		err := provider.Add(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
+
 		exists, err := provider.Exists(toAdd.EdgeControllerId)
 		gomega.Expect(err).To(gomega.Succeed())
 		gomega.Expect(exists).To(gomega.BeTrue())
+
+		_ = provider.Remove(toAdd.EdgeControllerId)
 	})
 
-	ginkgo.It("should be able to update an EIC", func(){
+	ginkgo.It("should be able to update an EIC", func() {
 		toAdd := CreateTestEdgeController()
+
 		err := provider.Add(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
+
 		toAdd.Name = "newName"
 		err = provider.Update(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
+
+		_ = provider.Remove(toAdd.EdgeControllerId)
 	})
 
-	ginkgo.It("should be able to retrieve an EIC", func(){
+	ginkgo.It("should be able to retrieve an EIC", func() {
 		toAdd := CreateTestEdgeController()
+
 		err := provider.Add(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
+
 		exists, err := provider.Exists(toAdd.EdgeControllerId)
 		gomega.Expect(err).To(gomega.Succeed())
 		gomega.Expect(exists).To(gomega.BeTrue())
+
 		retrieved, err := provider.Get(toAdd.EdgeControllerId)
 		gomega.Expect(err).To(gomega.Succeed())
 		gomega.Expect(retrieved).To(gomega.Equal(toAdd))
+
+		_ = provider.Remove(toAdd.EdgeControllerId)
 	})
 
-	ginkgo.It("should be able to list the EIC of an organization", func(){
-	    numEIC := 10
-	    organizationID := entities.GenerateUUID()
-	    for index := 0; index < numEIC; index ++{
-	    	toAdd := CreateTestEdgeController()
-	    	toAdd.OrganizationId = organizationID
+	ginkgo.It("should be able to list the EIC of an organization", func() {
+		var createdEIC []entities.EdgeController
+		numEIC := 10
+		organizationID := entities.GenerateUUID()
+
+		for index := 0; index < numEIC; index++ {
+			toAdd := CreateTestEdgeController()
+			toAdd.OrganizationId = organizationID
 			err := provider.Add(*toAdd)
+			createdEIC = append(createdEIC, *toAdd)
 			gomega.Expect(err).To(gomega.Succeed())
 		}
-	    // Add elements to other organizations
-		for index := 0; index < numEIC; index ++{
+
+		// Add elements to other organizations
+		for index := 0; index < numEIC; index++ {
 			toAdd := CreateTestEdgeController()
 			err := provider.Add(*toAdd)
+			createdEIC = append(createdEIC, *toAdd)
 			gomega.Expect(err).To(gomega.Succeed())
 		}
+
 		retrieved, err := provider.List(organizationID)
 		gomega.Expect(err).To(gomega.Succeed())
 		gomega.Expect(len(retrieved)).To(gomega.Equal(numEIC))
+
+		for i := 0; i < len(createdEIC); i++ {
+			_ = provider.Remove(createdEIC[i].EdgeControllerId)
+		}
+
 	})
 
-	ginkgo.It("should be able to remove an EIC", func(){
+	ginkgo.It("should be able to remove an EIC", func() {
 		toAdd := CreateTestEdgeController()
+
 		err := provider.Add(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
+
 		err = provider.Remove(toAdd.EdgeControllerId)
 		gomega.Expect(err).To(gomega.Succeed())
+
 		exists, err := provider.Exists(toAdd.EdgeControllerId)
 		gomega.Expect(err).To(gomega.Succeed())
 		gomega.Expect(exists).To(gomega.BeFalse())
 	})
-	ginkgo.It("should be able to update an EIC", func(){
+
+	ginkgo.It("should be able to update an EIC", func() {
 		toAdd := CreateTestEdgeController()
 		err := provider.Add(*toAdd)
 		gomega.Expect(err).To(gomega.Succeed())
@@ -90,7 +123,7 @@ func RunTest(provider Provider) {
 		gomega.Expect(ec).NotTo(gomega.BeNil())
 		gomega.Expect(ec.Name).Should(gomega.Equal("new Name"))
 
-
+		_ = provider.Remove(toAdd.EdgeControllerId)
 	})
 
 }
