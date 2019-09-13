6896d44e81dea07b23ed0a34633334c570934e35
Carmen de Lope <cdelope@daisho.group>
2019-09-13 11:54:31 +0200

    NP-1949 uncomment application mockup tests

--- internal/pkg/provider/application/mockup_test.go
+++ internal/pkg/provider/application/mockup_test.go
@@ -5,7 +5,7 @@ import "github.com/onsi/ginkgo"
 var _ = ginkgo.Describe("Mockup Application provider", func(){
 
 
-	//sp := NewMockupOrganizationProvider()
-	//RunTest(sp)
+	sp := NewMockupOrganizationProvider()
+	RunTest(sp)
 
 })
